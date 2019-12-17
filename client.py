c537f22ffb1ca5aab05d23e9199e9899a4723300
Muhammad Feroz <mferoz.bscs17seecs@seecs.edu.pk>
2019-12-17 22:06:50 +0000

    Making Client to enable listen at multple ports

--- client.py
+++ client.py
@@ -1,12 +1,45 @@
+import sys
+import threading
 import socket
+import  argparse
+count = 1
+# Making Custom Thread
 
-clientSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+class myThread(threading.Thread):
+    def __init__(self,sockfd):
+        threading.Thread.__init__(self)
+        self.sockfd = sockfd
+        self.num = count
+        count+=1
+    def run(self):
+        while True:
+            getData = self.sockfd.recv(1024)
+            print("From Server ",self.count,getData)
 
-port = 999
 
-clientSock.connect(('localhost',port))
-msg  = clientSock.recv(1024)
 
-clientSock.close()
+# Handling arguments passing  ::::::::::::::::::::::
+my_parser = argparse.ArgumentParser()
+my_parser.add_argument('-i', action='store', required=True)
+my_parser.add_argument('-r', action='store_true', required=False)
+my_parser.add_argument('-o', action='store', required=True)
+my_parser.add_argument('-a', action='store', required=True)
+my_parser.add_argument('-p', action='store',nargs='+',type=int, required=True)
+args = my_parser.parse_args()
+# End of Handling Arguments
+HOST = args.a # Extracting Server IP Address
 
-print(msg.decode('ascii'))
+portnumbers = args.p  # Extracting port numbers
+clientSockets = list()
+
+
+for i in range(len(portnumbers)):
+    clientSockets.append(socket.socket(socket.AF_INET,socket.SOCK_STREAM))
+    clientSockets[i].bind((HOST,portnumbers[i]))
+    newThread = myThread(clientSockets[i])
+    newThread.start
+    
+for i in range(len(portnumbers)):
+    print(clientSockets[i])
+
+
