0119f2c92065dfdffa8fe1f657975d847e5f4532
Muhammad Feroz <mferoz.bscs17seecs@seecs.edu.pk>
2019-12-22 12:17:53 +0000

    Adding handling multiples clients and servers through threading

--- client.py
+++ client.py
@@ -2,6 +2,12 @@ import sys
 import threading
 import socket
 import  argparse
+import signal
+import os
+import tqdm
+
+
+
 count = 1
 # Making Custom Thread
 
@@ -9,12 +15,15 @@ class myThread(threading.Thread):
     def __init__(self,sockfd):
         threading.Thread.__init__(self)
         self.sockfd = sockfd
+        global count
         self.num = count
         count+=1
     def run(self):
+        print("Thead Fuck You")
         while True:
             getData = self.sockfd.recv(1024)
-            print("From Server ",self.count,getData)
+            print("From Server ",self.num,getData)
+        self.sockfd.close()
 
 
 
@@ -30,14 +39,30 @@ args = my_parser.parse_args()
 HOST = args.a # Extracting Server IP Address
 
 portnumbers = args.p  # Extracting port numbers
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 clientSockets = list()
 
 
 for i in range(len(portnumbers)):
     clientSockets.append(socket.socket(socket.AF_INET,socket.SOCK_STREAM))
-    clientSockets[i].bind((HOST,portnumbers[i]))
+    clientSockets[i].connect((HOST,portnumbers[i]))
     newThread = myThread(clientSockets[i])
-    newThread.start
+    newThread.start()
+    newThread.join()
     
 for i in range(len(portnumbers)):
     print(clientSockets[i])
--- server.py
+++ server.py
@@ -1,14 +1,60 @@
+import sys
+import threading
 import socket
+import  argparse
+import signal
 
-serverSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-port = 999
+import os
+import tqdm
+count = 1
+# Making Custom Thread
 
-serverSock.bind(('',port))
-serverSock.listen(10)
+class myThread(threading.Thread):
+    def __init__(self,serverfd):
+        threading.Thread.__init__(self)
+        self.serverfd = serverfd
+        global count
+        self.num = count
+        count+=1
+    def run(self):
+        print("Server Has Starting")
+        self.serverfd.listen()
+        conn,addr = self.serverfd.accept()
+        with conn:
+            print("Connected by ",addr)
+            while True:
+                data = conn.recv(1024)
+                print(data.decode())
+            
+            
 
-while True:
-    clientSock, addr = serverSock.accept()
-    print("Got Connection from %s" % str(addr))
-    msg = "Yes Yes Yes Thank YOu"
-    clientSock.send(msg.encode('ascii'))
+
+
+# Handling arguments passing  ::::::::::::::::::::::
+my_parser = argparse.ArgumentParser()
+my_parser.add_argument('-i', action='store', required=True)
+my_parser.add_argument('-n', action='store', required=True, type=int)
+my_parser.add_argument('-f', action='store', required=True ,type=str)
+my_parser.add_argument('-p', action='store',nargs='+',type=int, required=True)
+args = my_parser.parse_args()
+# End of Handling Arguments
+HOST = "127.0.0.1"
+
+portnumbers = args.p  # Extracting port numbers
+
+numberOfServers = args.n # Extracting number of Servers
+serverSockets = list()
+
+
+for i in range(numberOfServers):
+    serverSockets.append(socket.socket(socket.AF_INET,socket.SOCK_STREAM))
+    serverSockets[i].setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serverSockets[i].bind((HOST,portnumbers[i]))
+    newThread = myThread(serverSockets[i])
+    newThread.start()
+    newThread.join()
     
+for i in range(len(portnumbers)):
+    print(serverSockets[i])
+
+
