a217c8022598e67896d88930d5692873c188a04c
Muhammad Feroz <mferoz.bscs17seecs@seecs.edu.pk>
2020-02-20 09:27:41 +0000

    This is Final Project for my Networking Course

--- client.py
+++ client.py
@@ -4,25 +4,47 @@ import socket
 import  argparse
 import signal
 import os
-import tqdm
+from tqdm import tqdm
+import shutil
 
-
-
+os.mkdir("/root/IDM/A")
 count = 1
 # Making Custom Thread
 
+videoParts = []
+
+lock = threading.Lock()
+
 class myThread(threading.Thread):
-    def __init__(self,sockfd):
+    def __init__(self,id,sockfd):
         threading.Thread.__init__(self)
         self.sockfd = sockfd
+        self.id=id
         global count
         self.num = count
         count+=1
+        self.daemon = True;
+        self.daemon = False
+        partnum = self.id
+        self.filename = os.path.join('A', ('part%04d.mp4' % partnum))
     def run(self):
+        part = open(self.filename,"+ab")
         print("Thead Fuck You")
+        file_size = int(self.sockfd.recv(2025).decode())
+        print(file_size)
+        progress = tqdm(total=int(file_size))
         while True:
-            getData = self.sockfd.recv(1024)
-            print("From Server ",self.num,getData)
+            if file_size < 0:
+                break
+            getData = self.sockfd.recv(2024)
+            part.write(getData)
+            file_size = file_size- 2024
+            #print(file_size)
+            progress.update(len(getData))
+        part.close()
+        with lock:
+            with open(self.filename,"rb") as part:
+                videoParts.append(part.read())
         self.sockfd.close()
 
 
@@ -44,27 +66,59 @@ portnumbers = args.p  # Extracting port numbers
 
 
 
-
-
-
-
-
-
-
-
-
-
 clientSockets = list()
-
+threads = []
 
 for i in range(len(portnumbers)):
     clientSockets.append(socket.socket(socket.AF_INET,socket.SOCK_STREAM))
     clientSockets[i].connect((HOST,portnumbers[i]))
-    newThread = myThread(clientSockets[i])
+    newThread = myThread(i,clientSockets[i])
     newThread.start()
-    newThread.join()
+    threads.append(newThread)
     
 for i in range(len(portnumbers)):
     print(clientSockets[i])
 
 
+while True:
+    inn = input(":")
+    if(inn == "resume"):
+        print("Resume")
+    elif inn == "pause":
+        print("Pause")
+    elif inn == "exit":
+        break
+
+
+
+for t in threads:
+    t.join()
+
+#with open("kakaka.mp4","wb") as f:
+#   for i in videoParts:
+#        print(i)
+#        f.write(i)
+
+
+readsize = 1024
+
+def join(fromdir, tofile):
+    print(fromdir)
+    print(tofile)
+    output = open(tofile, 'wb')
+    parts  = os.listdir(fromdir)
+    print(os.listdir)
+    parts.sort(  )
+    for filename in parts:
+        filepath = os.path.join(fromdir, filename)
+        fileobj  = open(filepath, 'rb')
+        while 1:
+            filebytes = fileobj.read(readsize)
+            if not filebytes: break
+            output.write(filebytes)
+        fileobj.close(  )
+    output.close()
+
+
+join("A","Hurrah.mp4")
+shutil.rmtree("/root/IDM/A")
--- server.py
+++ server.py
@@ -3,32 +3,39 @@ import threading
 import socket
 import  argparse
 import signal
+import time
 
 import os
 import tqdm
 count = 1
+
+
+kilobytes = 1024
+megabytes = kilobytes * 1000
+chunksize = int(3 * megabytes)
 # Making Custom Thread
 
 class myThread(threading.Thread):
-    def __init__(self,serverfd):
+    def __init__(self,id,serverfd,data):
         threading.Thread.__init__(self)
         self.serverfd = serverfd
         global count
         self.num = count
         count+=1
+        self.id = id
+        self.data = data
     def run(self):
         print("Server Has Starting")
         self.serverfd.listen()
         conn,addr = self.serverfd.accept()
+        conn.sendall((str(len(self.data))).encode())
+        time.sleep(3)
         with conn:
             print("Connected by ",addr)
+            conn.sendall(self.data)
             while True:
                 data = conn.recv(1024)
-                print(data.decode())
             
-            
-
-
 
 # Handling arguments passing  ::::::::::::::::::::::
 my_parser = argparse.ArgumentParser()
@@ -46,15 +53,38 @@ numberOfServers = args.n # Extracting number of Servers
 serverSockets = list()
 
 
+# video spliting
+
+chunks = []
+
+filename = "split.mp4"
+filesize = os.path.getsize(filename)
+print(numberOfServers)
+chunksize = int(filesize/(numberOfServers))
+inputs = open(filename, 'rb')         
+partnum = 0          
+while 1:
+    chunk = inputs.read(chunksize)
+    if not chunk: break 
+    chunks.append(chunk)
+    partnum  = partnum+1                                              
+inputs.close( )
+
+
+
+threads = []
+print(len(chunks))
 for i in range(numberOfServers):
     serverSockets.append(socket.socket(socket.AF_INET,socket.SOCK_STREAM))
     serverSockets[i].setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     serverSockets[i].bind((HOST,portnumbers[i]))
-    newThread = myThread(serverSockets[i])
+    newThread = myThread(i,serverSockets[i],chunks[i])
     newThread.start()
-    newThread.join()
+    threads.append(newThread)
     
 for i in range(len(portnumbers)):
     print(serverSockets[i])
 
 
+for t in threads:
+    t.join()
